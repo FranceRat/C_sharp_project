e2ca762e25f764bbc86a0dd89ba92f0f95937fd6
FranceRat <nakagawa.shota.nl2@is.naist.jp>
2021-08-20 16:10:52 +0900

    Resize処理を追加する

--- 画像処理系/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ 画像処理系/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,6 +33,13 @@
             this.OpenImgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.excutetion_button = new System.Windows.Forms.Button();
+            this.resize_height = new System.Windows.Forms.TextBox();
+            this.resize_width = new System.Windows.Forms.TextBox();
+            this.resize_height_label = new System.Windows.Forms.Label();
+            this.resize_width_label = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -77,9 +84,77 @@
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Resize",
+            "XXX"});
+            this.comboBox1.Location = new System.Drawing.Point(604, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropOpen);
+            this.comboBox1.DropDownClosed += new System.EventHandler(this.comboBox1_Dropclosed);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "実行する処理";
+            // 
+            // excutetion_button
+            // 
+            this.excutetion_button.Location = new System.Drawing.Point(604, 425);
+            this.excutetion_button.Name = "excutetion_button";
+            this.excutetion_button.Size = new System.Drawing.Size(214, 23);
+            this.excutetion_button.TabIndex = 4;
+            this.excutetion_button.Text = "実行";
+            this.excutetion_button.UseVisualStyleBackColor = true;
+            this.excutetion_button.Click += new System.EventHandler(this.excutetion_button_Click);
+            // 
+            // resize_height
+            // 
+            this.resize_height.Location = new System.Drawing.Point(747, 156);
+            this.resize_height.Name = "resize_height";
+            this.resize_height.Size = new System.Drawing.Size(71, 19);
+            this.resize_height.TabIndex = 5;
+            // 
+            // resize_width
+            // 
+            this.resize_width.Location = new System.Drawing.Point(631, 156);
+            this.resize_width.Name = "resize_width";
+            this.resize_width.Size = new System.Drawing.Size(72, 19);
+            this.resize_width.TabIndex = 6;
+            // 
+            // resize_height_label
+            // 
+            this.resize_height_label.AutoSize = true;
+            this.resize_height_label.Location = new System.Drawing.Point(602, 159);
+            this.resize_height_label.Name = "resize_height_label";
+            this.resize_height_label.Size = new System.Drawing.Size(17, 12);
+            this.resize_height_label.TabIndex = 7;
+            this.resize_height_label.Text = "高さ";
+            // 
+            // resize_width_label
+            // 
+            this.resize_width_label.AutoSize = true;
+            this.resize_width_label.Location = new System.Drawing.Point(714, 159);
+            this.resize_width_label.Name = "resize_width_label";
+            this.resize_width_label.Size = new System.Drawing.Size(27, 12);
+            this.resize_width_label.TabIndex = 8;
+            this.resize_width_label.Text = "x 幅";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(874, 610);
+            this.ClientSize = new System.Drawing.Size(874, 606);
+            this.Controls.Add(this.excutetion_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -103,6 +178,13 @@
         private System.Windows.Forms.ToolStripMenuItem OpenImgToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button excutetion_button;
+        private System.Windows.Forms.TextBox resize_height;
+        private System.Windows.Forms.TextBox resize_width;
+        private System.Windows.Forms.Label resize_height_label;
+        private System.Windows.Forms.Label resize_width_label;
     }
 }
 
--- 画像処理系/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 画像処理系/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -18,9 +18,14 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         Image image;
+        Mat cv_img;
+        Dictionary<string, List<System.Windows.Forms.Control>> rem_add_Controls;
         public Form1()
         {
             InitializeComponent();
+            rem_add_Controls = new Dictionary<string, List<Control>>() {
+                {"Resize",new List<Control>(){resize_width_label,resize_height_label,resize_height,resize_width} }
+            };
         }
         /// <summary>
         /// 
@@ -42,13 +47,75 @@ namespace WindowsFormsApp1
             // リソース開放セナずっとメモリに残りっぱなしでやばいことになる
             if (image != null) image.Dispose();
             //画像パスからファイル読み込み
-            Mat cv_img = new Mat(ofd.FileName);
-            image = BitmapConverter.ToBitmap(cv_img);
+            cv_img = new Mat(ofd.FileName);
+            Mat disp_img = new Mat();
+            OpenCvSharp.Size disp_size = new OpenCvSharp.Size(pictureBox2.Width, pictureBox2.Height);
+            Cv2.Resize(cv_img, disp_img, disp_size);
+            image = BitmapConverter.ToBitmap(disp_img);
             pictureBox2.Image = image;
 
 
         }
+        /// <summary>
+        /// コンボボックスのpullパネルが開かれた際の挙動
+        /// 現在選択されている者に対応するコントロールを削除する(非表示にする)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_DropOpen(object sender, EventArgs e)
+        {
+            string selected = (string)comboBox1.SelectedItem;
+            if (selected == null || !rem_add_Controls.ContainsKey(selected)) return;
+            foreach (Control T in rem_add_Controls[selected])
+            {
+                Controls.Remove(T);
+            }
+        }
+        /// <summary>
+        /// コンボボックスが開かれた際に
+        /// 選択したテキストに対応したコントロールを作成する(追加する)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_Dropclosed(object sender, EventArgs e)
+        {
+            string selected = (string)comboBox1.SelectedItem;
+            if (selected == null || !rem_add_Controls.ContainsKey(selected)) return;
+            foreach (Control T in rem_add_Controls[selected])
+            {
+                Controls.Add(T);
+            }
+        }
 
-
+        private void excutetion_button_Click(object sender, EventArgs e)
+        {
+            string selected = (string)comboBox1.SelectedItem;
+            switch (selected)
+            {
+                case "Resize":
+                    int w;
+                    int h;
+                    if (image == null)
+                    {
+                        MessageBox.Show("ファイルから画像開いといてくれ頼むわ", "画像開かれとらんよ", MessageBoxButtons.OK);
+                        return;
+                    } 
+                    if(!Int32.TryParse(resize_width.Text,out w))
+                    {
+                        MessageBox.Show("ハイ雑魚！！！！お前は整数が打てないのか？\n小学生からやり直せ!!!!", "サイズ変更で整数以外はダメよ", MessageBoxButtons.OK);
+                        return;
+                    }
+                    if (!Int32.TryParse(resize_height.Text, out h))
+                    {
+                        MessageBox.Show("ハイ雑魚！！！！お前は整数が打てないのか？\n小学生からやり直せ!!!!", "サイズ変更で整数以外はダメよ", MessageBoxButtons.OK);
+                        return;
+                    }
+                    OpenCvSharp.Size resize_size = new OpenCvSharp.Size(w, h);
+                    Mat dst = new Mat();
+                    Cv2.Resize(cv_img, dst, resize_size);
+                    Cv2.ImShow(selected, dst);
+                    break;
+            }
+        }
     }
 }
